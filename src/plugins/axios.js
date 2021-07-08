626d345dfbc4827f0e5e0421f2ebf4645ff83a4b
jirenmaa <jirenmaa@gmail.com>
2021-07-08 13:31:14 +0700

    fix

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let BASEURL = null;
-if (process.env.NODE_ENV === "Production") {
+if (process.env.NODE_ENV === "production") {
   BASEURL = "https://api-incov-19.herokuapp.com";
 } else {
   BASEURL = "http://localhost:8000";
